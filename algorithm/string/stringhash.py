3a8a55789a14c39a98c0cb385b5ef6c263205228
unlessbamboo <unlessbamboo@gmail.com>
2016-02-18 16:08:47 +0800

    update algorithm

---
+++ algorithm/string/stringhash.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#coding:utf8
+##
+# @file stringhash.py
+# @brief    hash算法（散列算法、摘要算法）和hash表的统称
+#               1,hash——一种数据编码方式
+#                   将大尺寸的数据浓缩到一个数字中，从而方便的实现
+#                   数据匹配、数据查找功能、密码校验
+#                   数据编码方式有：
+#                       csv编码、json编码、base编码、md5(hash)、sha(hash)
+#               2，为何会提升速度？
+#                   大数据匹配非常慢，固定长度数据的匹配当然非常快，笨蛋
+#               3，缺点
+#                   可能存在某些不同的大数据经过编码之后得到一模一样的
+#                   hash
+#               4，混淆
+#                   md5本身不仅仅作为加密，仅仅是一种编码方式.
+#               5，hash表构建过程中的hash函数
+#
+#           hash表
+#               1，hash-table——根据key-value进行查找的数据结构，该数据
+#                   结构中存放key-value的一条条记录
+#
+#           用途：
+#               1，海量数据查找TOP10（用于很多地方）
+#               2，
+# @author unlessbamboo
+# @version 1.0
+# @date 2016-02-18
+
+
+if __name__ == '__main__':
+    """main"""
+    pass
