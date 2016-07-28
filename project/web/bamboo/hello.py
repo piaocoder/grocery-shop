1f4b8d8412b735ce15059b55e928ddd933f41f16
unlessbamboo <unlessbamboo@gmail.com>
2016-07-28 18:41:20 +0800

    Test ssh key when add coding's ssh key

--- project/web/bamboo/hello.py
+++ project/web/bamboo/hello.py
@@ -4,17 +4,46 @@ from flask import Flask, request, make_response, redirect
 from flask import render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import (StringField, SubmitField)
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "I am a secret key"
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField("What is your name?",
+                       validators=[Required()])
+    submit = SubmitField("submit")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     user_agent = request.headers.get('User-Agent')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
     return render_template(
-        'index.html', user_agent=user_agent)
+        'index.html', user_agent=user_agent,
+        form=form, name=name)
 
 
 @app.route('/cookie/<name>/')
